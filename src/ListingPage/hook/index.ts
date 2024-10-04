d4b5b48ce5b8c9fe54df1a9ddc6e864095f4a032
shadowbattalion <hafizspaceumar@gmail.com>
2024-10-04 23:14:43 +0800

    added comments

--- src/ListingPage/hook/index.ts
+++ src/ListingPage/hook/index.ts
@@ -10,6 +10,8 @@ export default function usePaginateAndSearch (setList:React.Dispatch<React.SetSt
   
     const fetchList = async (page:number) => {
     try {
+        // fetches data from the backend which will respond the list of entries and the total number of pages 
+        // after dividing the total number of entries with page size, which is the amount of entries that will appear per page.
         const url=`http://localhost:9000/api/list/get?searchKey=${searchKey}&page=${page}&pageSize=25`
         const response = await axios.get<AxiosGetType>(url); 
         const { list, totalPages } = response.data;
@@ -22,28 +24,35 @@ export default function usePaginateAndSearch (setList:React.Dispatch<React.SetSt
     }
 
     useEffect(() => {
+        // Will fetch the list from backend when usePaginateAndSearch hook is called. Will be called if there is a change during page change or search key word changes
         fetchList(currentPage);
       }, [currentPage, searchKey]);
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // when the state change for search key, the component will reload and do a fetch via useEffect
         setSearchKey(e.target.value)
+        // will reset the current page because during email search, the number of possible matches will reduce and hence the pagination number.
+        // There for it is logical to reset the current page to 1
         setCurrentPage(1);
     }
 
     const prev = () => {
+      // as long as the current page is not the first page, this handle will bring user to the previous page
       if (currentPage > 1) {
         setCurrentPage(currentPage- 1);
       }
     };
   
     const next = () => {
+      // as long as the current page is 1 page before the total number of pages, this handle will bring user to the next page
       if (currentPage < totalPages) {
         setCurrentPage(currentPage + 1);
       }
     };
   
     const current = (pageNumber:number) => {
-      
+        // this handle to be sent to Panel where it will reside in each pagination button so that users can
+        // click on a certain page number and go to it.
         setCurrentPage(pageNumber);
   
     };
--- src/UploadListPage/hook/useHandleUpload.ts
+++ src/UploadListPage/hook/useHandleUpload.ts
@@ -6,6 +6,7 @@ export default function  useHandleUpload(){
 
     const [file, setFile] = useState<File|null>()
 
+    //handle change for the file input element
     const handleChange =  async (e: React.ChangeEvent<HTMLInputElement>) => {
         const fileList:FileList|null = e.target.files
         if(fileList){
@@ -17,6 +18,9 @@ export default function  useHandleUpload(){
     const handleSubmit = async(e: React.FormEvent<HTMLFormElement>, setUploadOutcome:React.Dispatch<React.SetStateAction<string>>, setUploadProgress:React.Dispatch<React.SetStateAction<number>>) => {
       e.preventDefault()
       if(file){
+        //takes the file and add to the formdata to format it for http request.
+        // multipart/form-data is for big data like files
+        // onUploadProgress is where the current data loaded is calculated and is passed to the frontend progress bar 
         const url = 'http://localhost:9000/api/list/';
         const formData = new FormData();
         formData.append('file', file);
@@ -32,11 +36,12 @@ export default function  useHandleUpload(){
         };
 
         const response = await axios.post(url, formData, config)
-        // Axios throwing Network Error despite MSW running and hitting the handlers.
-        // (for testing) Whole flow works during testing. Use the following and comment out the above statement. 
-        // const response = {data:{"test":test}} 
+        // Note: Axios throwing Network Error despite MSW running and hitting the handlers.
+        
+        // (the for testing) Use the following and comment out the above statement. 
+        // const response = {data:{"test":test}} //Whole flow works when the axios is bypassed
 
-
+        // depending on the response, this label will show up on the frontend to indicate to users if the file upload was successful
         response.data?setUploadOutcome("File uploaded successfully"):setUploadOutcome("Fail to upload")
         
       }else{
